9e9c51c740b0219d3ec378a5c1ce4e9d280c972e
Altair1ONE <inbox2hammad@gmail.com>
2025-09-20 18:16:16 +0500

    Full site redeploy with latest updates

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -1,16 +1,16 @@
 // please update required information
 const siteMetadata = {
-    title: 'Next.js Blog With Tailwind CSS and Contentlayer',
-    author: 'CodeBucks',
-    headerTitle: 'Next.js Blog',
-    description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
+    title: 'The Modern Explorer',
+    author: 'Altair1ONE',
+    headerTitle: 'Altair1ONE blog',
+    description: 'A blog created with Next.js, Tailwind.css and Velite.',
     language: 'en-us',
     theme: 'system', // system, dark or light
     siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
     siteLogo: '/logo.png',
     socialBanner: '/social-banner.png', // add social banner in the public folder
-    email: 'codebucks27@gmail.com', 
-    github: 'https://github.com/codebucks27',
+    email: 'inbox2hammad@gmail.com', 
+    github: 'https://github.com/Altair1ONE',
     twitter: 'https://twitter.com/code_bucks',
     facebook: 'https://facebook.com',
     youtube: 'https://youtube.com/codebucks',
